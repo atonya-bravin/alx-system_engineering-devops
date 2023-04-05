8cfc875244890c3e06a8f816bdaf223b0ca8afec
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-05 09:17:19 +0000

    installs flask from pip3 using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,11 @@
 # Install a package using Puppet
 
-exec { 'install pip':
-  command => '/usr/bin/apt-get install -y pip'
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip install flask -v 2.1.0'
-}
-
-package { 'pip':
+package { 'python3-pip':
   ensure => 'installed',
-  before => Exec['install pip']
 }
 
-package { 'flask':
-  ensure  => 'installed',
-  before  => Exec['install flask'],
-  require => Package['pip']
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask -v 2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
