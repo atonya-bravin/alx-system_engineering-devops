03bb02b215173e7622499f1af7f2bed15f73c956
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-24 19:10:35 +0000

    script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,29 +15,29 @@ if __name__ == '__main__':
     user_uri = '{api}/users/{user_id}'.format(api=api_url, user_id=emp_id)
     todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
 
-    # User Response
+    """ User Response """
     res = requests.get(user_uri).json()
 
-    # Name of the employee in question
+    """ Name of the employee in question """
     name = res.get('name')
 
-    # User TODO_LIST Response
+    """ User TODO_LIST Response """
     res = requests.get(todo_uri).json()
 
-    # Total number of tasks i.e. both completed and non-completed tasks
+    """ Total number of tasks i.e. both completed and non-completed tasks"""
     total = len(res)
 
-    # Number of non-completed tasks
+    """ Number of non-completed tasks """
     non_completed = sum([elem['completed'] is False for elem in res])
 
-    # Number of completed tasks
+    """ Number of completed tasks """
     completed = total - non_completed
 
-    # Formatting the expected output
+    """ Formatting the expected output """
     str = "Employee {emp_name} is done with tasks({completed}/{total}):"
     print(str.format(emp_name=name, completed=completed, total=total))
 
-    # Printing completed tasks
+    """ Printing completed tasks """
     for elem in res:
         if elem.get('completed') is True:
             print("\t {}".format(elem.get('title')))
