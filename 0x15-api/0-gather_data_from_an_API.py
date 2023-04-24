872081c205630e3fedd89da1b1b22ec813b08c3e
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-24 19:21:11 +0000

    script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-"""Given an Employee ID, returns information
-about his/her TODO list progress.
-"""
+"""fetching json data from an api"""
+
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    try:
-        emp_id = int(argv[1])
-    except ValueError:
-        exit()
+if __name__ == "__main__":
 
-    api_url = 'https://jsonplaceholder.typicode.com'
-    user_uri = '{api}/users/{user_id}'.format(api=api_url, user_id=emp_id)
-    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
+    """ get the user id from the shell"""
+    user_id = argv[1]
 
-    """ User Response """
-    res = requests.get(user_uri).json()
+    """ user route """
+    user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
 
-    """ Name of the employee in question """
-    name = res.get('name')
+    """ user dictionary from user route """
+    user_dict = requests.get(user_url).json()
 
-    """ User TODO_LIST Response """
-    res = requests.get(todo_uri).json()
+    """ get name from dict """
+    user_name = user_dict.get("name")
 
-    """ Total number of tasks i.e. both completed and non-completed tasks"""
-    total = len(res)
+    """ todo route """
+    user_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-    """ Number of non-completed tasks """
-    non_completed = sum([elem['completed'] is False for elem in res])
+    """ user dictionaty from todo route """
+    user_todo = user_todo.json()
+    total_todo = 0
+    completed_titles = []
+    number_completed = 0
 
-    """ Number of completed tasks """
-    completed = total - non_completed
+    for item in user_todo:
+        if item.get("userId") == int(user_id):
+            total_todo += 1
+            if item.get("completed") is True:
+                number_completed += 1
+                completed_titles.append(item.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name, number_completed, total_todo)
 
-    """ Formatting the expected output """
-    str = "Employee {emp_name} is done with tasks({completed}/{total}):"
-    print(str.format(emp_name=name, completed=completed, total=total))
-
-    """ Printing completed tasks """
-    for elem in res:
-        if elem.get('completed') is True:
-            print("\t {}".format(elem.get('title')))
+    """ printing completed tasks """
+    for title in completed_titles:
+        print("\t {}".format(title))
