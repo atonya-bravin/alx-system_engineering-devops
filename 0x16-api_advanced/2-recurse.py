81213568b74652f4238b16d4c0caa2e07fee4648
= <=>
2023-05-12 07:53:52 +0000

    correction of the indentation

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """
 this script contains a recursive function that queries the Reddit API
-and returns a list containing the titles of all hot articles for a given subreddi
+and returns a list containing the titles of all hot articles for a
+given subreddit
 """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-        """ The querying recursive function"""
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {'User-Agent': 'API Agent'}
-        params = {'after': after} if after else None
-        response = requests.get(url, headers=headers, params=params)
+    """ The querying recursive function"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'API Agent'}
+    params = {'after': after} if after else None
+    response = requests.get(url, headers=headers, params=params)
 
-        # check whether the response is OK
-        if response.status_code != 200:
-            return None
+    # check whether the response is OK
+    if response.status_code != 200:
+        return None
 
-        # process the response data
-        data = response.json()
-        children = data.get("data").get("children")
-        for child in children:
-            hot_list.append(child.get("data").get("title"))
+    # process the response data
+    data = response.json()
+    children = data.get("data").get("children")
+    for child in children:
+        hot_list.append(child.get("data").get("title"))
 
-        # check for more pages
-        after = data.get("data").get("after")
-        if after is not None:
-            recurse(subreddit, hot_list, after)
+    # check for more pages
+    after = data.get("data").get("after")
+    if after is not None:
+        recurse(subreddit, hot_list, after)
 
-        return hot_list
+    return hot_list
