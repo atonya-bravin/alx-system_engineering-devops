9d9f230ee0c5f50548e1cf299ab1938226eb9ecc
= <=>
2023-05-12 07:28:08 +0000

    script that contains a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that query the reddit api and retrive first 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'API Agent'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
+        else:
+            print(None)
