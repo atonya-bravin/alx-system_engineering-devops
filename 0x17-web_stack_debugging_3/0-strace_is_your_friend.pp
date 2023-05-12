9986f6167670610c348e8f2b5b2e15fcd0966f9f
= <=>
2023-05-12 09:16:09 +0000

    style corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,12 @@
-# when using strace, we found that the error was that there was a file being called with the path: /var/www/html/wp-includes/class-wp-locale.phpp
-# This file is however not present. Using, grep -rl class-wp-locale.phpp * we find out that the file is being called in the 
+# when using strace, we found that the error was that there was a file being
+# called with the path: /var/www/html/wp-includes/class-wp-locale.phpp
+# This file is however not present. Using, grep -rl class-wp-locale.phpp * 
+# we find out that the file is being called in the 
 # following file: /var/www/html/wp-includes/wp-settings.php
-# on checking whether /var/www/html/wp-includes/class-wp-locale.php exists using ls class-wp-locale.php we find out that it exists.
-# This script therefore changes class-wp-locale.phpp to class-wp-locale.php in wp-settings.php
+# on checking whether /var/www/html/wp-includes/class-wp-locale.php exists
+# using ls class-wp-locale.php we find out that it exists.
+# This script therefore changes class-wp-locale.phpp to class-wp-locale.php 
+# in wp-settings.php
 
 $file_pth = '/var/www/html/wp-settings.php'
 exec{ 'replace_line':
