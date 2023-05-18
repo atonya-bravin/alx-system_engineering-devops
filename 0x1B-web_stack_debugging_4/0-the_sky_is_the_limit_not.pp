136125b7c7166ba3b12617dea7b271d3ec43aaca
= <=>
2023-05-18 20:45:49 +0000

    puppet script containing solution

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# change the ULIMIT of the nginx server
+$file_pth = '/etc/default/nginx'
+exec{ 'change_limit':
+  command => "sed -i 's/15/4096/g' ${file_pth}",
+  path    => ['/bin','/usr/bin']
+}
+exec{ 'restart_nginx':
+  command => "sudo service nginx restart",
+  path    => ['/bin','/usr/bin']
+}
