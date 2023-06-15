c68e99527a06f8aa4f8e346c04d90eabb6dae836
= <=>
2023-06-15 23:21:56 +0000

    Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-"""fetching json data from an api"""
+"""export data in the CSV format in an API"""
 
-import csv
-import json
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    user_id = argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
-    user_dict = requests.get(user_url).json()
-    user_name = user_dict.get("username")
-    user_todo = requests.get("{}/todos".format(user_url))
-    user_todo = user_todo.json()
-    file_name = user_id + ".json"
+if __name__ == '__main__':
+    employeeId = argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    my_dict = {}
+    response = requests.get(url)
+    username = response.json().get('username')
 
-    my_dict[user_id] = []
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
 
-    for elem in user_todo:
-        inner_dict = {}
-        inner_dict["task"] = elem.get("title")
-        inner_dict["completed"] = elem.get("completed")
-        inner_dict["username"] = user_name
-        my_dict.get(user_id).append(inner_dict)
-
-    with open(file_name, 'w') as f:
-        json.dump(my_dict, f)
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
